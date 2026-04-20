8a7da77241046feb2998cc30f7109f0dc18cda91
Nankare <karetailangnan@gmail.com>
2026-04-20 20:31:40 +0900

    Update index.js

--- index.js
+++ index.js
@@ -4,8 +4,8 @@ const line = require("@line/bot-sdk");
 const app = express();
 
 const config = {
-  channelAccessToken: process.env.r6LLHE7K3W74vnw1v53TTIM495jeFXGAt7Ulta7o5ISgtxUhTUxR8aU0Si0m77JOM84PPA0zautslbbfIr5HaM+E0+NYyRV6LKqnvDqaoHXDL1iW6jpElUJN1U1FL4D/TuMy2s3Evnw29JEUkZqhfQdB04t89/1O/w1cDnyilFU=,
-  channelSecret: process.env.781d3db1e8ac356e0182e1c68b5b329b
+  channelAccessToken: process.env.CHANNEL_ACCESS_TOKEN,
+  channelSecret: process.env.CHANNEL_SECRET
 };
 
 const client = new line.Client(config);
